6482f5b996ff1798f8b764fd9d1be57a12f7f880
Uddeshy Manoj Jaiswal <uddeshkrrish@gmail.com>
2022-05-07 17:45:39 +0530

    lastttime

--- cart.js
+++ cart.js
@@ -7,13 +7,18 @@ var newdata = JSON.parse(localStorage.getItem("newcart"))|| [];
 
 data.forEach(function(ele){
     
-    var img = document.querySelector("#img");
-    img.src = ele.image_url;
+    var img = document.createElement("img");
+   img.src = ele.image_url;
+
+
     var h2 = document.querySelector("#disc>h2");
      h2.innerText = ele.name;
-     var p = document.querySelector("#disc>h2+p");
+     var p = document.querySelector("#fun>p");
     p.innerText = ele.strprice;
-     var p2 = document.querySelector("#disc>h2+p+p");
+     var p2 = document.querySelector("#fun>p+p");
      p2.innerText = ele.price;
+
+    var box =  document.querySelector("#img")
+  box.append(img);
     
 })
